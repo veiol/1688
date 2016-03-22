052f9eafd41eb40f8c4e2717b5d4feb0125b7cfa
veiol <veiol@qq.com>
2016-03-22 23:25:29 +0800

    init app

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php
-echo "Hello word!";
-?>
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+error_reporting(E_ALL & ~E_NOTICE);
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+// 定义应用目录
+define('APP_PATH','./App/');
+
+//关闭定义安全文件
+define('BUILD_DIR_SECURE', false);
+
+//绑定Tianpinjiameng模块
+//define('BIND_MODULE','Tianpinjiameng');
+
+// 绑定访问Index控制器
+//define('BIND_CONTROLLER','Index');
+	
+// 引入ThinkPHP入口文件
+require '../ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
